35111357c33e7b2e031e088bcd6fda3dccd15322
kato1999 <kato.1999.cs@gmail.com>
2020-08-10 23:04:04 +0900

    質問募集フォームの公開

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
-//スマホの時だけ電話番号有効化
-//スムーススクロール
+// //スマホの時だけ電話番号有効化
+// //スムーススクロール
 
-$(function(){
-	$('a[href^="#"]').click(function(){
-		var speed = 500;
-		var href= $(this).attr("href");
-		var target = $(href == "#" || href == "" ? 'html' : href);
-		var position = target.offset().top;
-		$("html, body").animate({scrollTop:position}, speed, "swing");
-		return false;
-	});
-});
+// $(function(){
+// 	$('a[href^="#"]').click(function(){
+// 		var speed = 500;
+// 		var href= $(this).attr("href");
+// 		var target = $(href == "#" || href == "" ? 'html' : href);
+// 		var position = target.offset().top;
+// 		$("html, body").animate({scrollTop:position}, speed, "swing");
+// 		return false;
+// 	});
+// });
 
-/***************** MENU ******************/
+// /***************** MENU ******************/
 
-$(function() {
-  $(".navbar-toggle").click(function() {
-    $("#gnavi").slideToggle(200);
-    $(".icon-bar").toggleClass("closeup");
-    return false;
-  });
-});
+// $(function() {
+//   $(".navbar-toggle").click(function() {
+//     $("#gnavi").slideToggle(200);
+//     $(".icon-bar").toggleClass("closeup");
+//     return false;
+//   });
+// });
 
-//スマホのメニューをタップしたら閉じる
+// //スマホのメニューをタップしたら閉じる
 
-$(document).ready(function () {
- $(".navbar-nav li a").click(function(event) {
- $(".navbar-collapse").removeClass('show');
- });
-});
+// $(document).ready(function () {
+//  $(".navbar-nav li a").click(function(event) {
+//  $(".navbar-collapse").removeClass('show');
+//  });
+// });
